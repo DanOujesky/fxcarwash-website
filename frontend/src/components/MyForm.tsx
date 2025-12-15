92fd759cb486f6dbd01da0e7809ad7a6d1838b26
DanOujesky <oujesky@spsejecna.cz>
2025-12-15 13:54:45 +0100

    change MyForm title

--- frontend/src/components/MyForm.tsx
+++ frontend/src/components/MyForm.tsx
@@ -2,7 +2,7 @@ function MyForm({ children }: { children: React.ReactNode }) {
   return (
     <div className="bg-black w-full h-screen flex justify-center items-center">
       <div className=" flex justify-center items-center flex-col ">
-        <div className="text-white p-10 text-3xl">F.X Carwash</div>
+        <div className="text-white p-10 text-3xl">F.X. Carwash</div>
         <div className="bg-white p-5">
           <form className="flex flex-col gap-5  min-w-100">{children}</form>
         </div>
